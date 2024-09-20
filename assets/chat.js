944b856c76fe0a92d7fbb29092d2f7671e6c9973
martinberlin <martin@luckycloud.de>
2024-09-20 14:02:07 +0200

    Implement preloading GIFs

--- assets/chat.js
+++ assets/chat.js
@@ -1,6 +1,28 @@
+var loadpath = "assets/load/";
+
+function delay(time){
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+preload([
+    loadpath + "0.gif",loadpath + "1.gif",loadpath + "2.gif",loadpath + "3.gif",loadpath + "4.gif",loadpath + "5.gif",loadpath + "6.gif",
+    loadpath + "7.gif",loadpath + "8.gif",loadpath + "9.gif",loadpath + "10.gif",loadpath + "11.gif",loadpath + "12.gif",loadpath + "13.gif",
+    loadpath + "14.gif",loadpath + "15.gif",loadpath + "16.gif",loadpath + "17.gif",loadpath + "18.gif",loadpath + "19.gif",loadpath + "20.gif",
+    loadpath + "21.gif",loadpath + "22.gif",loadpath + "23.gif",loadpath + "24.gif",loadpath + "25.gif"
+]);
+
 $(document).ready(function() {
 
     $("#chat").click( function() {
+        var imgid = Math.floor(Math.random() * 25);
+        $("#preload").attr("src",loadpath + imgid +".gif");
+        $("#preload").show();
+
+
         var input = $("#input").val();
             console.log("Send request to API: "+input);
 
@@ -8,9 +30,10 @@ $(document).ready(function() {
             type: 'POST',
             data: { request: input },
             url: 'chat.php',
-            async: false,
+            async: true,
 
             success: function(result){
+                $("#preload").hide();
                 $("#response").html(result);
             },
 
