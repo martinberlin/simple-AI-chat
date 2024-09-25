a1fc46839e60c235ba76fdc22380d20164d3eaf5
martinberlin <martin@luckycloud.de>
2024-09-25 23:03:09 +0200

    Start with image upload

--- config.php
+++ config.php
@@ -7,6 +7,9 @@ $dotenv->load();
 
 $apiurl = "https://models.inference.ai.azure.com/chat/completions";
 
+// IMAGE Uploads
+$upload_dir = './uploads/';
+$ai_image_describe = "What is in the image?";
 $tem_chat = <<<EOT
 {
         "messages": [
@@ -32,10 +35,21 @@ $tem_img = <<<EOT
             },
             {
                 "role": "user",
-                "content": [{"text": "What''s in this image?", "type": "text"}, 
+                "content": [{"text": "$ai_image_describe", "type": "text"}, 
                 {"image_url": {"url":"_IMAGEURL_","detail":"medium"}, "type": "image_url"}]
             }
         ],
-        "model": "gpt-4o-mini"
+        "model": "gpt-4o"
 }
 EOT;
+// OTHER Useful tips
+$phpFileUploadErrors = array(
+    0 => 'There is no error, the file uploaded with success',
+    1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+    2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+    3 => 'The uploaded file was only partially uploaded',
+    4 => 'No file was uploaded',
+    6 => 'Missing a temporary folder',
+    7 => 'Failed to write file to disk.',
+    8 => 'A PHP extension stopped the file upload.',
+);
--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+require("config.php");
+$upload_satus = false;
+$upload_error = "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_FILES['image']['error'] >0) {
+        $upload_error = $phpFileUploadErrors[$_FILES['image']['error']];
+    }
+    $file_ext = explode('/', $_FILES['image']['type']);
+    $uploadfile = $upload_dir.time().".".$file_ext[1];
+
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
+        $upload_satus = true;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,10 +22,14 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/openfile.js"></script>
+    <script src="assets/openfile.js?t=<?=time() ?>"></script>
     <script src="assets/chat.js"></script>
 
-    <link rel="stylesheet" href="assets/styles.css">
+    <link rel="stylesheet" href="assets/styles.css?t=<?=time() ?>">
+    <script>
+        var upload_status = <?=$upload_satus?>
+        var upload_status = <?=$upload_satus?>
+    </script>
 </head>
 
 
@@ -32,8 +50,19 @@
                     <img class="avatar" src="assets/img/ai.png">
                     <div class="media-body">
                         <p>Hi</p>
+                        <?php if ($upload_satus) { ?>
+                        <div class="row">
+                            <div class="col-md-4">
+                            <b><?=$upload_error?></b>
+                        <p>This is your uploaded image:<br>
+                            <img style="width:40%" src="<?=$uploadfile ?>">
+                        </p>
+                            </div>
+                        </div>
+                        <?php } else { ?>
                         <p>I will reply using <b>gpt-4o-mini</b> model. It's only a single reply per question, I won't keep the conversation going like ChatGTP
                         </p>
+                        <?php } ?>
                     </div>
                 </div>
 
@@ -41,17 +70,22 @@
                 <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;"><div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; height: 0px; right: 2px;"><div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 2px;"></div></div></div>
 
             <div class="row">
+
                 <div class="publisher bt-1 border-light col-md-10">
                     <img class="avatar avatar-xs" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="...">
-                    <textarea id="input" class="form-control" rows="3" placeholder="A S K"></textarea>
+                    <textarea id="input" class="form-control" rows="3" placeholder="A S K"><?=($upload_satus)?$ai_image_describe:'' ?></textarea>
                     <button id="chat" class="publisher-btn text-info" href="#" data-abc="true"><i class="fa fa-paper-plane"></i></button>
 
                     <span class="publisher-btn file-group">
-                          <button class="fa fa-paperclip file-browser" id="openFileButton" title="Analyze image and deliver a description using AI"></button>
-                          <input type="file">
-                        </span>
+                        <form action="index.php" id="upload" method="post" enctype="multipart/form-data">
+                                <!-- MAX_FILE_SIZE must precede the file input field -->
+                            <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+                            <input id="selectFile" type="file" name="image" title="Analyze image and deliver a description using AI" >
+                        </form>
+                    </span>
 
                 </div>
+
             </div>
 
         </div>
